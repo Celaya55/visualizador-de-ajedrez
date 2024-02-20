b261cf8cb83c2a43aca2c9e2e0922e2d7947aa3a
Celaya55 <158091587+Celaya55@users.noreply.github.com>
2024-02-20 12:45:57 -0700

    Add files via upload

--- Visualizador-ajedrez-main/visualizador/js/funciones.js
+++ Visualizador-ajedrez-main/visualizador/js/funciones.js
@@ -1,8 +1,237 @@
 var contadorPaso = 0;
-var turno=true;
-var i=0;
-var j=0;
-/*True es para las blancas y false para las negras*/ 
+var j = 0;
+/*True es para las blancas y false para las negras*/
+var tokens = "", i = 0 , turno = true;
+var texto = "";
+var columna="";
+var renglon="";
+var posicion="";
+var jaque="";
+function reglas() {
+  alert(`Notación de piezas:
+
+    Rey: K
+    Reina: Q
+    Torre: R
+    Alfil: B
+    Caballo: N
+    Peón: no se indica ninguna letra (por ejemplo, e4)
+    Notación de columnas:
+    
+    Las columnas se indican con letras de la "a" a la "h". La columna más a la izquierda es la "a" y la columna más a la derecha es la "h".
+    Notación de filas:
+    
+    Las filas se indican con números del 1 al 8. El lado de las blancas está en las filas 1 y 2, mientras que el lado de las negras está en las filas 7 y 8.
+    Notación de movimientos:
+    
+    Se utiliza la notación abreviada de la pieza seguida de la casilla a la que se mueve. Por ejemplo, Re4 significa que el Rey se mueve a la casilla e4.
+    Capturas:
+    
+    Cuando una pieza captura a otra, se utiliza "x" para indicar la captura. Por ejemplo, Bxe4 significa que el Alfil captura en la casilla e4.
+    Enroque:
+    
+    El enroque corto se indica con O-O y el enroque largo se indica con O-O-O.
+    Peones:
+    
+    Cuando un peón avanza sin capturar, solo se indica la casilla de destino. Por ejemplo, e4 significa que el peón se mueve a e4.
+    Promoción de peones:
+    
+    Cuando un peón alcanza la octava fila, se promociona a otra pieza (generalmente una reina). La promoción se indica agregando la letra de la pieza deseada después del movimiento del peón. Por ejemplo, e8=Q significa que el peón en e8 se promociona a una reina.
+    Jaque:
+    
+    Se indica con el símbolo "+" al final del movimiento. Por ejemplo, Nf7+ significa que el Caballo ha dado jaque.
+    Jaque mate:
+    
+    Se indica con el símbolo "#" al final del movimiento. Por ejemplo, Qh8# significa que la Reina ha dado jaque mate.`)
+}
+function removernumero() {
+  // var texto = document.getElementById("textarea1").value;
+  //document.getElementById("textarea2").innerHTML = texto.replace(/\d+\.\s*/g, ''); 
+   texto = document.getElementById("textarea1").value.replace(/\d+\.\s*/g, '');
+}
+function getTokens() {
+  tokens = texto.split(/\s+/);
+
+  //var salida = "";
+  //for (let i = 0; i < tokens.length; i++) {
+//salida += tokens[i] + "\n"
+      
+  //}
+  //document.getElementById("out").innerHTML = salida;
+}
+function pasos(){
+  var pieza="";
+  if (!tieneMayusculas(tokens[i])) {
+    pieza="peon";
+    jaque="";
+if(tokens[i].startsWith("ex")){
+  posicion = tokens[i].slice(2).split(/(\d+)/);
+  columna=posicion[0].replace('x', '');
+  renglon=posicion[1];
+}else{
+    posicion = tokens[i].split(/(\d+)/);
+    columna=posicion[0].replace('x', '');
+    renglon=posicion[1];
+}
+if(tokens[i].endsWith("+")){
+  jaque="Jaque";
+  posicion = tokens[i].slice(1).split(/(\d+)/);
+  columna=posicion[0].replace('x', '');
+  renglon=posicion[1].slice(-1);
+  
+}
+if(tokens[i].endsWith("#")){
+  jaque="Jaque Mate";
+  posicion = tokens[i].slice(1).split(/(\d+)/);
+  columna=posicion[0].replace('x', '');
+  renglon=posicion[1].slice(-1);
+  
+}
+  }
+  else if(tokens[i].startsWith("N")){
+    pieza="caballo";
+    jaque="";
+    if(tokens[i].startsWith("Nx")){
+      posicion = tokens[i].slice(2).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1];
+    }else{
+    posicion = tokens[i].slice(1).split(/(\d+)/);
+    columna=posicion[0].replace('x', '');
+    renglon=posicion[1];
+    }
+    if(tokens[i].endsWith("+")){
+      jaque="Jaque";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+    if(tokens[i].endsWith("#")){
+      jaque="Jaque Mate";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+  }
+  else if(tokens[i].startsWith("B")){
+    pieza="Alfil";
+    jaque="";
+    if(tokens[i].startsWith("Bx")){
+      posicion = tokens[i].slice(2).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1];
+    }else{
+    posicion = tokens[i].slice(1).split(/(\d+)/);
+    columna=posicion[0].replace('x', '');
+    renglon=posicion[1];
+    }
+    if(tokens[i].endsWith("+")){
+      jaque="Jaque";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+    if(tokens[i].endsWith("#")){
+      jaque="Jaque Mate";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+  }
+  else if(tokens[i].startsWith("K")){
+    pieza="Rey";
+    jaque="";
+    if(tokens[i].startsWith("Kx")){
+      posicion = tokens[i].slice(2).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1];
+    }else{
+    posicion = tokens[i].slice(1).split(/(\d+)/);
+    columna=posicion[0].replace('x', '');
+    renglon=posicion[1];
+    }
+    if(tokens[i].endsWith("+")){
+      jaque="Jaque";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+    if(tokens[i].endsWith("#")){
+      jaque="Jaque Mate";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+  }
+  else if(tokens[i].startsWith("Q")){
+    pieza="Reina";
+    jaque="";
+    if(tokens[i].startsWith("Qx")){
+      posicion = tokens[i].slice(2).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1];
+    } else {
+    posicion = tokens[i].slice(1).split(/(\d+)/);
+    columna=posicion[0].replace('x', '');
+    renglon=posicion[1].replace('x', '');
+    }
+    if(tokens[i].endsWith("+")){
+      jaque="Jaque";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+    if(tokens[i].endsWith("#")){
+      jaque="Jaque Mate";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+  }
+  else if(tokens[i].startsWith("R")){
+    pieza="Torre";
+    jaque="";
+    if(tokens[i].startsWith("Rx")){
+      posicion = tokens[i].slice(2).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1];
+    } else {
+    posicion = tokens[i].slice(1).split(/(\d+)/);
+    columna=posicion[0].replace('x', '');
+    renglon=posicion[1];
+    }
+    if(tokens[i].endsWith("+")){
+      jaque="Jaque";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+    if(tokens[i].endsWith("#")){
+      jaque="Jaque Mate";
+      posicion = tokens[i].slice(1).split(/(\d+)/);
+      columna=posicion[0].replace('x', '');
+      renglon=posicion[1].slice(-1);
+      
+    }
+  }
+  else if(tokens[i].startsWith("O")){
+    pieza="Enroque";
+    jaque="";
+  }
+  document.getElementById("turno").innerHTML = "Movimiento: " + (i + 1) 
+  + " Turno " + (turno == true ? "blancas " : " negras ")+ tokens[i++]+ " "+ pieza   
+  +" y se va a mover a Columna:"+columna+" Renglon:"+renglon+" "+jaque;
+  turno = !turno;
+}
 
 function iniciar() {
   var celdas = document.getElementById("Tablero");
@@ -123,63 +352,60 @@ function cargarPartida() {
     /*document.getElementById("texto").value = document.getElementById("cargarBoton").files;*/
   };
   scanner.readAsText(archivo);
+  removernumero();
+  getTokens();
 }
 
-async function  pasoApaso() {
+ function pasoApaso() {
   var celdas = document.getElementById("texto").value;
-  /*celdas.rows[2].cells[1].style = "background-image: none; background-size:cover;";
-  celdas.rows[3].cells[1].style = "background-image: url(./img/PeonN.png); background-size:cover;";
-  switch(contadorPaso){
-    case 0:
-      celdas.rows[2].cells[1].style = "background-image: none; background-size:cover;";
-      celdas.rows[3].cells[1].style = "background-image: url(./img/PeonN.png); background-size:cover;";
-      break;
-    case 1:
-      celdas.rows[2].cells[2].style = "background-image: none; background-size:cover;";
-      celdas.rows[3].cells[2].style = "background-image: url(./img/PeonN.png); background-size:cover;";
-      break;
-  }
-  contadorPaso++;*/
-
   var tabla = document.getElementById("Tablero");
-  var lineas = celdas.split("\n");
+  //var lineas = celdas.split("\n");
   //for (let i = 0; i < lineas.length; i++) {
-    //alert(lineas[i]);
-    var tokens = lineas[i].split(" ");
-    //for (let j = 1; j < tokens.length; j++) {
-      //alert(tokens[j]);
-      //alert(tieneMayusculas(tokens[j]));
-      j=1;
-      if (!tieneMayusculas(tokens[j])) {
-        //alert("Si soy un Peon y me moví a "+tokens[j]);
-        var posicion = tokens[j].split(/(\d+)/);
-        //El usar alert no se recomienda porque no puedes hacer nada en la página, se recomienda usar el console.log
-        console.log(
-          "Columna " +
-            convertirLetraNumero(posicion[0]) +
-            " renglon " +
-            posicion[1]
-        );
-        var columna = parseInt(convertirLetraNumero(posicion[0]));
-        var renglon = parseInt(posicion[1]);
-        if(turno){
-          tabla.rows[renglon+1].cells[columna+1].style =
-        
-          "background-image: url(./img/PeonB.png); background-size:cover;";
-        }else{
-          tabla.rows[renglon+1].cells[columna+1].style =
-        
-          "background-image: url(./img/PeonN.png); background-size:cover;";
-        
-        } 
-      }
-      turno=!turno;
-      document.getElementById("turno").innerHTML = "Turno de las "+(turno?"blancas ":"negras " )+renglon+" "+columna;
-      await new Promise((resolve) => setTimeout(resolve, 3000)) ;
-    //}
+  //alert(lineas[i]);
+  var tokens = celdas.split(/\s+/);
+ 
+  //alert(tokens[j]);
+  //alert(tieneMayusculas(tokens[j]));
+ 
+  //if (turno) {
+    //j = 1;
+  //} else {
+   // j = 2;
   //}
-i++;
-  
+  /*if (!tieneMayusculas(tokens[j])) {
+    //alert("Si soy un Peon y me moví a "+tokens[j]);
+    var posicion = tokens[j].split(/(\d+)/);
+    //El usar alert no se recomienda porque no puedes hacer nada en la página, se recomienda usar el console.log
+    console.log(
+      "Columna " + convertirLetraNumero(posicion[0]) + " renglon " + posicion[1]
+    );
+    var columna = parseInt(convertirLetraNumero(posicion[0]));
+    var renglon = parseInt(posicion[1]);
+    if (turno) {
+      tabla.rows[renglon + 1].cells[columna + 1].style =
+        "background-image: url(./img/PeonB.png); background-size:cover;";
+    } else {
+      tabla.rows[renglon - 1].cells[columna + 1].style =
+        "background-image: url(./img/PeonN.png); background-size:cover;";
+    }
+  }
+  if (turno == false) {
+    i++;
+  }*/
+  turno = !turno;
+  document.getElementById("turno").innerHTML =
+    "Turno de las " +
+    (turno ? "blancas " : "negras ") +
+    //renglon +
+    " " +
+    //columna +
+    " " +
+    tokens[j];
+  //await new Promise((resolve) => setTimeout(resolve, 3000));
+  //}
+  //}
+  i++;
+  j++;
 }
 
 function tieneMayusculas(cadena) {
@@ -221,11 +447,11 @@ function convertirLetraNumero(letra) {
   }
 }
 
-async function test() {
+/*async function test() {
   var salida = "";
   for (let i = 0; i < 10; i++) {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     salida += i + "<br>";
     document.getElementById("out").innerHTML = salida;
   }
-}
+}*/
